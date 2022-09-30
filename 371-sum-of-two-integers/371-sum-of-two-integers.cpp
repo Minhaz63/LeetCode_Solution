f2bb5c8e59581c01c31727fe3e14d61cc791b761
Md.Minhazul Islam <48880006+Minhaz63@users.noreply.github.com>
2022-09-30 20:09:23 +0600

    Time: 5 ms (11.71%), Space: 6 MB (25.67%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -3,7 +3,17 @@ public:
     int getSum(int a, int b) {
         if (b == 0)
         return a;
-    else
-        return getSum( a ^ b,(unsigned) (a & b) << 1);;
+        
+        unsigned c;
+        
+        while(b!=0){
+            c=a&b;    // find the carry
+            a = a^b;  // find sum without carry
+            b = c<<1; // left shift carry so that we can add the carry with next bit
+        }
+        
+       
+        return a;
+        
     }
 };
